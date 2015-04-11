a8ccd4574485618868f090e9e17f53ac75767d28
Alex Kneipp <alexzippy517@gmail.com>
2015-04-11 09:44:42 -0400

    Added movable interface for all components that need to move.  Also, forgot to push last commit, so see changelog for full list of changes in version 1.2 Indev.

---
+++ CGS Culminating/src/com/canine505/util/Movable.java
@@ -0,0 +1,20 @@
+package com.canine505.util;
+
+/**
+ * @author Alex Kneipp
+ * Interface for all movable visual components
+ *
+ */
+public interface Movable 
+{
+	//should return the x and y values of the position in array indicies 0 and 1.
+	public double[] getPosition();
+	//should change x and y based on gravitational forces from other components and current velocity
+	public void updatePosition();
+	//should return the velocity in the manner of a velocity object
+	public Velocity getVelocity();
+	//should change the current velocity of the object based on collisions and the gravity of other objects
+	public void updateVelocity();
+	//should return true if it has collided with another component and false if it has not
+	public boolean getHasCollided();
+}
