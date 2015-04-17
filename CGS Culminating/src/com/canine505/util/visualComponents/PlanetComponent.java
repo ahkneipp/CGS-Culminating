0ed7f6bb874ee59926a65001de6f83908b54958a
Alex Kneipp <alexzippy517@gmail.com>
2015-04-16 20:57:20 -0400

    Start tracking PlanetComponent

---
+++ CGS Culminating/src/com/canine505/util/visualComponents/PlanetComponent.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.canine505.util.visualComponents;
+
+import com.canine505.util.Movable;
+/**
+ * @author alex
+ *
+ */
+public class PlanetComponent extends MatterComponent implements Movable 
+{
+
+}
